5f79c20df2fd2605bdd605eac6a25fce9bb45eda
Jorge <jorgekoolma1@hotmail.com>
2024-05-10 01:47:43 -0700

    Pagina de venta de productos variados, primer commit

---
+++ js/producto.js
@@ -0,0 +1,95 @@
+function redireccionarTelevision() {
+    // Puedes redirigir a una página web externa
+    window.location.href = '../Productos/Television.html';
+  }
+
+  function redireccionarAudifonos() {
+    window.location.href = '../Productos/Audifonos.html';
+
+  }
+
+  function redireccionarTablet() {
+    window.location.href = '../Productos/Tablet.html';
+
+  }
+
+  function redireccionarAppleWatch() {
+    window.location.href = '../Productos/RelojApple.html';
+
+  }
+
+  function redireccionarNike() {
+    window.location.href = '../Productos/TenisNike.html';
+
+  }
+
+  function redireccionarReloj() {
+    window.location.href = '../Productos/Reloj.html';
+
+  }
+
+  function redireccionarAsusTUFROG() {
+    window.location.href = '../Productos/AsusTufRog.html';
+
+  }
+
+  function redireccionarPerfume() {
+    window.location.href = '../Productos/Perfume.html';
+
+  }
+
+
+  function redireccionarMSIRaider() {
+    window.location.href = '../Productos/MsiRaider.html';
+
+  }
+
+  function redireccionarMSIKatana() {
+    window.location.href = '../Productos/MsiKatana.html';
+
+  }
+
+  function redireccionarLenovoLegion5() {
+    window.location.href = '../Productos/LenovoLegion5.html';
+
+  }
+
+  function redireccionarLenovoLegion() {
+    window.location.href = '../Productos/LenovoLegion.html';
+
+  }
+
+  function redireccionarLaptopGamerMsi() {
+    window.location.href = '../Productos/LaptopGamerMsi.html';
+
+  }
+  
+  function redireccionarhp15() {
+    window.location.href = '../Productos/hp15.html';
+
+  }
+
+  function redireccionariphone() {
+    window.location.href = '../Productos/iphone.html';
+
+  }
+
+  function redireccionarAsusFX() {
+    window.location.href = '../Productos/AsusFX.html';
+
+  }
+  
+  function redireccionarAsusRogStrix() {
+    window.location.href = '../Productos/AsusRogStrix.html';
+
+  }
+
+  function redireccionarAsusTuf() {
+    window.location.href = '../Productos/AsusTuf.html';
+
+  }
+
+
+
+
+  
